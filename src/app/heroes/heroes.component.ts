19b46cc47d67b7e260ff7d00f86f81424d9851af
Ashley <ashleymspires@gmail.com>
2018-05-16 14:36:17 -0700

    upload

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  heroes:Hero[];
+  selectedHero:Hero;
+
+  constructor(private heroService:HeroService) { }
+
+  ngOnInit() {
+    this.getHeroesInComponent();
+  }
+  onSelect(hero:Hero){
+    this.selectedHero = hero;
+  }
+  getHeroesInComponent():void {
+    // this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe((data)=>{
+      this.heroes = data;
+  })
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Superman'},
+  {id: 12, name: 'Batman'},
+  {id: 13, name: 'Thor'},
+  {id: 14, name: 'IronMan'},
+  {id: 15, name: 'Hulk'},
+  {id: 16, name: 'Captian America'},
+  {id: 17, name: 'Deadpool'},
+  {id: 18, name: 'AntMan'},
+  {id: 19, name: 'Magma'},
+  {id: 20, name: 'Tornado'}
+
+];
